4112c5d37fc93140fc8798d36142c907a645c3c9
Muskan <muskanchhabra.0806@gmail.com>
2024-07-14 12:52:51 +0530

    fone

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,21 +3,21 @@ export const projects = [
         title : "Quizzy",
         subtitle : "HTML, CSS, JS",
         description:"This is a quiz application where one can participate in quiz and check his knowledge level with scores. ",
-        image:"/public/assets/Untitled design (11).png",
+        image:"../assets/Untitled design (11).png",
         link:""
     },
     {
         title : "Forecaster",
         subtitle : "HTML, CSS, JS",
         description:"You can check the weather of any area using this website and some bonus tips according to weather.",
-        image:"/public/assets/Untitled design (10).png",
+        image:"../assets/Untitled design (10).png",
         link:""
     },
     {
         title : "PasswordGen",
         subtitle : "HTML, CSS, JS",
         description:"This can be used for generating secure and random password for any website with a strong password. ",
-        image:"/public/assets/Untitled design (12).png",
+        image:"../assets/Untitled design (12).png",
         link:""
     }
     
