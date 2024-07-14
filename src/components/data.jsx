c477b68be6f774aef17a743364b54bc73b7fbca0
Muskan <muskanchhabra.0806@gmail.com>
2024-07-14 12:54:29 +0530

    doit

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -3,21 +3,21 @@ export const projects = [
         title : "Quizzy",
         subtitle : "HTML, CSS, JS",
         description:"This is a quiz application where one can participate in quiz and check his knowledge level with scores. ",
-        image:"../assets/Untitled design (11).png",
+        image:"/src/assets/Untitled design (11).png",
         link:""
     },
     {
         title : "Forecaster",
         subtitle : "HTML, CSS, JS",
         description:"You can check the weather of any area using this website and some bonus tips according to weather.",
-        image:"../assets/Untitled design (10).png",
+        image:"/src/assets/Untitled design (10).png",
         link:""
     },
     {
         title : "PasswordGen",
         subtitle : "HTML, CSS, JS",
         description:"This can be used for generating secure and random password for any website with a strong password. ",
-        image:"../assets/Untitled design (12).png",
+        image:"/src/assets/Untitled design (12).png",
         link:""
     }
     
