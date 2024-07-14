0f0b4022a48db19b50496a8967633de7702020a7
Muskan <muskanchhabra.0806@gmail.com>
2024-07-14 13:00:11 +0530

    cpdp

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,23 +1,28 @@
+import image1 from '../assets/Untitled design (11).png'
+import image2 from '../assets/Untitled design (10).png'
+
+import image3 from '../assets/Untitled design (12).png'
+
 export const projects = [
     {
         title : "Quizzy",
         subtitle : "HTML, CSS, JS",
         description:"This is a quiz application where one can participate in quiz and check his knowledge level with scores. ",
-        image:"/src/assets/Untitled design (11).png",
+        image:image1,
         link:""
     },
     {
         title : "Forecaster",
         subtitle : "HTML, CSS, JS",
         description:"You can check the weather of any area using this website and some bonus tips according to weather.",
-        image:"/src/assets/Untitled design (10).png",
+        image:image2,
         link:""
     },
     {
         title : "PasswordGen",
         subtitle : "HTML, CSS, JS",
         description:"This can be used for generating secure and random password for any website with a strong password. ",
-        image:"/src/assets/Untitled design (12).png",
+        image:image3,
         link:""
     }
     
